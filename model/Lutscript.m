3538753ea2f48033168881647bbbd37a1839f3a0
Dave-rcc <davide.rocco.93@gmail.com>
2020-05-13 19:12:15 +0200

    Controller: First matlab function implementation

--- model/Lutscript.m
+++ model/Lutscript.m
@@ -1,6 +1,8 @@
-lut = eye(20,13)*0;
-for j = 1:10        % angolo
-    for i = 1:13    % velocità
-        lut(((j-1)*2+1):((j-1)*2+2),i) = LinPlant(j*3,i*10);
+Klut = zeros(10,13,2);
+%Klut = eye(20,13)*0;
+for i = 1:10        % angolo
+    for j = 1:13    % velocità
+        %Klut(((j-1)*2+1):((j-1)*2+2),i) = LinPlant(j*3,i*10);
+        Klut(i,j,:) = LinPlant(i*3,j*10);
     end
 end
