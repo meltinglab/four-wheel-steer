b1049bc4ec94bc6a843f6ec14bc8407190018eb6
Filip Valgimigli <42498829+delta512@users.noreply.github.com>
2020-05-29 18:08:27 +0200

    Simulation: fix "deltaR" logging and some naming aesthetics

--- model/SimRoutine.m
+++ model/SimRoutine.m
@@ -19,6 +19,21 @@ VEH = getVariable(get_param(bdroot('CRReferenceApplication'), 'modelworkspace'),
 % Generate LUTs based on current vehicle parameters
 [Klut, Eqlut] = LUTScript(VEH, maxAngle, resAngle, maxSpeed, resSpeed);
 
+
+% Simulate plain vehicle
+set_param(switchBlock{1},'Value','0');
+save_system('CRReferenceApplication',[],'SaveDirtyReferencedModels','on');
+
+sim('CRReferenceApplication');
+
+deltaROL = logsout.find('SteerRearCtrl');
+tOL = deltaROL.Values.Time(:,1);
+deltaROL = deltaROL.Values.Data(:,1);
+betaUOL = logsout.find('BetaU');
+betaUOL = betaUOL.Values.Data(:,1);
+omegaZOL = logsout.find('OmegaZ');
+omegaZOL = omegaZOL.Values.Data(:,1);
+
 % Simulate with Active Rear Steering
 set_param(switchBlock{1},'Value','1');
 save_system('CRReferenceApplication',[],'SaveDirtyReferencedModels','on');
@@ -28,26 +43,13 @@ sim('CRReferenceApplication');
 deltaRRS = logsout.find('SteerRearCtrl');
 tRS = deltaRRS.Values.Time(:,1);
 deltaRRS = deltaRRS.Values.Data(:,1);
-betaURS = logsout.find('betaU');
+betaURS = logsout.find('BetaU');
 betaURS = betaURS.Values.Data(:,1);
-omegaZRS = logsout.find('BodyAngleDerivatives');
-omegaZRS = omegaZRS.Values.Data(:,3);
-
-% Simulate plain vehicle
-set_param(switchBlock{1},'Value','0');
-save_system('CRReferenceApplication',[],'SaveDirtyReferencedModels','on');
-
-sim('CRReferenceApplication');
-
-deltaROL = logsout.find('betaU');
-tOL = deltaROL.Values.Time(:,1);
-deltaROL = deltaROL.Values.Data(:,1);
-betaUOL = logsout.find('betaU');
-betaUOL = betaUOL.Values.Data(:,1);
-omegaZOL = logsout.find('BodyAngleDerivatives');
-omegaZOL = omegaZOL.Values.Data(:,3);
+omegaZRS = logsout.find('OmegaZ');
+omegaZRS = omegaZRS.Values.Data(:,1);
 
 
+% Plot data
 RearSteeringVSOL = figure('Name','Rear Steering vs non Rear Steering','NumberTitle','off')
 
 subplot(2,2,1);
@@ -57,10 +59,10 @@ legend('RS','OL')
 
 subplot(2,2,2);
 plot(tRS,betaURS,'g',tOL, betaUOL, 'r')
-title('betaU Comparison')
+title('BetaU Comparison')
 legend('RS','OL')
 
 subplot(2,2,[3,4]);
 plot(tRS, deltaRRS,'g',tOL, deltaROL, 'r')
-title('deltaR Comparison')
+title('DeltaR Comparison')
 legend('RS','OL')
