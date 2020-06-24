b6f4eccf33af35d7b3917447c05d4a781a4f3b4f
Filip Valgimigli <42498829+delta512@users.noreply.github.com>
2020-06-24 09:58:03 +0200

    Controller: fix first row null values in both LUTs

--- model/LUTScript.m
+++ model/LUTScript.m
@@ -5,7 +5,7 @@ function [Klut, Eqlut] = LUTScript(vehicle, maxAngle, resAngle, maxSpeed, resSpe
 
     Klut = zeros(slotsAngle, slotsSpeed, 2);
     Eqlut = zeros(slotsAngle, slotsSpeed, 2);
-    for a = 2:slotsAngle
+    for a = 1:slotsAngle
         for v = 1:slotsSpeed
             [Klut(a,v,:), Eqlut(a,v,:)] = LinPlant((a-1)*resAngle, (v-1)*resSpeed, vehicle);
         end
