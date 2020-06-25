1135e161d9356ea6d5d6523af911fc4876480db5
Filip Valgimigli <42498829+delta512@users.noreply.github.com>
2020-06-25 17:25:39 +0200

    Project: update license to GPLv3 and synchronize LaTeX listings.

--- model/SimRoutine2.m
+++ model/SimRoutine2.m
@@ -6,6 +6,21 @@
 % - "LaneChange" per un doppio cambio di corsia
 % - "LaneChangeIce" per un doppio cambio di corsia con strada ghiacciata
 % - "ConstRadius" per una curva semplice
+%
+% Copyright (C) 2019-2020  Filippo Bellesia, Filippo Catellani, Davide Rocco, Filip Valgimigli
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 function simRoutine2(condition)
 
